81c36c03965b1958ac9ea4742a6fe9191e00466e
CKeogh <c.keogh.10.ck@gmail.com>
2019-05-21 15:13:55 +0100

    setup react app

---
+++ yozu-spike/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header">HEADER</div>
+      <div className="calendar">CALENDAR</div>
+      <div className="updatePoster">UPDATEPOSTER</div>
+    </div>
+  );
+}
+
+export default App;
