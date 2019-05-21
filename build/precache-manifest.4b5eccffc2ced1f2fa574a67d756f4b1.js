c4c6c99d526edb7ad92aa6670b47491a72f0e116
CKeogh <c.keogh.10.ck@gmail.com>
2019-05-21 20:27:32 +0100

    remove extra directory

---
+++ build/precache-manifest.4b5eccffc2ced1f2fa574a67d756f4b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6262ea9110570a1b94f992786d612e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62b5052080a9316f4dcf",
+    "url": "/static/css/main.916b282d.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "62b5052080a9316f4dcf",
+    "url": "/static/js/main.95707a34.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
